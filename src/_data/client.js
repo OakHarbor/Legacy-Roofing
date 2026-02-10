df5fe276d58cc0d2f5660649fe2e51b3b912173c
Brandon Kelley <brandon@kelley-family.org>
2026-02-10 12:54:58 -0800

    Index content finished, moving to other content pages.

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,24 +1,20 @@
 module.exports = {
-    name: "Sunnys Roofing",
-    email: "sunnysroofing08@gmail.com",
-    phoneForTel: "206-854-6718",
-    phoneFormatted: "(206) 854-6718",
+    name: "Legacy Roofing",
+    email: "admin@thelegacyroofers.com",
+    phoneForTel: "615-717-4383",
+    phoneFormatted: "(615) 717-4383",
     address: {
-        lineOne: "Edmonds, WA, United States, Washington",
-        city: "Edmonds",
-        state: "WA",
-        zip: "98204",
-        country: "US",
-        mapLink: "https://maps.app.goo.gl/9dN16FNEh432kkJx6",
+        city: "Nashville",
+        state: "TN",
+        mapLink: "https://maps.app.goo.gl/be6WnP81v8EX6c189",
     },
     socials: {
-        facebook: "https://www.facebook.com/p/Sunnys-roofing-LLC-100091642522299/",
+        facebook: "",
         instagram: "",
         youtube: "",
         x: ""
     },
-    //Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
-    domain: "https://example.com",
+    domain: "https://thelegacyroofers.com",
     // Passing the isProduction variable for use in HTML templates
     isProduction: process.env.ELEVENTY_ENV === "PROD",
 };
